ab9909d6f73f6aaedb12af44a70c0ab425d1d4c0
jiananti <jiananti@usc.edu>
2023-09-23 13:52:52 -0700

    update main

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,9 +6,8 @@ public class EnemyController : MonoBehaviour
 {
     public float moveSpeed = 2.0f;
     public float movementRange = 5.0f; // Adjust this range as needed
-    public LayerMask groundLayer;
 
-    private bool isMovingRight = true;
+    public bool isMovingRight = true;
     private Vector3 startingPosition;
     private Vector3 leftLimit;
     private Vector3 rightLimit;
@@ -22,28 +21,29 @@ public class EnemyController : MonoBehaviour
 
     private void Update()
     {
-        // Check if the enemy is grounded (touching the ground layer)
-        bool isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, groundLayer);
-
-        if (isGrounded)
+       // Move the enemy within the specified range
+        if (isMovingRight)
         {
-            // Move the enemy within the specified range
-            if (isMovingRight)
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+            if (transform.position.x >= rightLimit.x)
             {
-                transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
-                if (transform.position.x >= rightLimit.x)
-                {
-                    isMovingRight = false;
-                }
-            }
-            else
-            {
-                transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
-                if (transform.position.x <= leftLimit.x)
-                {
-                    isMovingRight = true;
-                }
+                isMovingRight = false;
             }
         }
+        else
+        {
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+            if (transform.position.x <= leftLimit.x)
+            {
+                isMovingRight = true;
+            }
+        }
+    }
+	
+	// Public property for isMovingRight
+    public bool IsMovingRight
+    {
+        get { return isMovingRight; }
+        set { isMovingRight = value; }
     }
 }
